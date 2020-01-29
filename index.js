dc82d1a2f0b8015bf890a52a9c9a5041ff3505be
Ojus Save <ojas931992@gmail.com>
2020-01-28 21:57:26 -0800

    Updated index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function dialogflow_req(query_text) {
     // A unique identifier for the given session
     const sessionId = uuid.v4();
     // getProjectId()
-    let projectId = 'zoom-agent-1-exafhv';
+    let projectId = 'dialogflow-project-id';
         // Create a new session
     const sessionClient = new dialogflow.SessionsClient();
     const sessionPath = sessionClient.sessionPath(projectId, sessionId);
@@ -25,63 +25,65 @@ async function dialogflow_req(query_text) {
 
     // Send request and log result
     const responses = await sessionClient.detectIntent(request);
-    console.log('Detected intent');
     const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-
-    let response = result.fulfillmentText
-    if (result.intent) {
-        console.log(`  Intent: ${result.intent.displayName}`);
-    } else {
-        console.log(`  No intent matched.`);
+    let response = ''
+    for(let i in result.fulfillmentMessages){
+        response += result.fulfillmentMessages[i].text.text[0] + "\n\n"
     }
     return response
 }
 
+
+function bot_run(){
 // get the lastest post from devforum
-request('http://devforum.zoom.us/posts.json', function (error, response, body) {
-    let latest_posts = JSON.parse(body).latest_posts;
-    let topics = new Set();
-    let categories = new Set([8])
-    let post
-    for(post in latest_posts){
-            if(!topics.has(latest_posts[post].topic_id)){
+    request('http://devforum.zoom.us/posts.json', function (error, response, body) {
+        let latest_posts = JSON.parse(body).latest_posts;
+        let topics = new Set();
+        let categories = new Set([8])
+        let post
+        for (post in latest_posts) {
+            if (!topics.has(latest_posts[post].topic_id)) {
                 topics.add(latest_posts[post].topic_id);
-                if(latest_posts[post].post_number == 1 &&
-                    categories.has(latest_posts[post].category_id)){
+                if (latest_posts[post].post_number == 1 &&
+                    categories.has(latest_posts[post].category_id)) {
                     // send request to dialogflow
-                    let query_text = "topic_id: "+latest_posts[post].topic_id+",category_id: "+latest_posts[post].category_id+",topic_title: "+latest_posts[post].topic_title;
-                    console.log("Query: "+query_text);
-                    dialogflow_req(query_text).then(function(response) {
+                    let topic_id = latest_posts[post].topic_id
+                    let query = (latest_posts[post].cooked.length > 256) ? latest_posts[post].topic_title : latest_posts[post].cooked;
+                    let category_id = latest_posts[post].category_id
+                    let query_text = "topic_id: " + topic_id + ",category_id: " + category_id + ",topic_title: " + query;
+                    console.log("Query: " + query_text);
+                    dialogflow_req(query_text).then(function (response) {
                         // response to devforum
-                        console.log(response)
+                        console.log(` Dialogflow Response : ${response}`)
                         let response_options = {
                             'method': 'POST',
-                            'url': 'http://devforum.zoom.us/posts.json',
+                            'url': 'https://devforum.zoom.us/posts',
                             'headers': {
-                                'Authorization': '',
-                                'Content-Type': 'application/json'
+                                'Content-Type': 'application/x-www-form-urlencoded',
+                                'Api-Key': 'your discourse api key',
+                                'Api-Username': "your discourse username"
                             },
-                            body: JSON.stringify({
-                                "topic_id": latest_posts[post].topic_id,
-                                "raw": response
-                            })
-                        }
-                        request(response_options, function (error, response) {
-                            console.log(response)
-                            if (error) {
-                                console.log(error)
+                            formData: {
+                                'topic_id': topic_id,
+                                'raw': response
                             }
+                        };
+                        request(response_options, function (error, response) {
+                            console.log(response.statusCode)
+                            // console.log(response)
+                            // if (error) {
+                            //     console.log(error)
+                            // }
                         })
                     })
                 }
             }
         }
-        // 1 minute pause for polling
-        sleep(60000).then()
-    // console.log('body:', JSON.parse(body).latest_posts);
-});
+    });
+    // console.log("Attempting again")
+    // bot_run()
+}
+
+bot_run()
 
 
-const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
